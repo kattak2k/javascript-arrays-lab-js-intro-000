07b72f6c5ccaa2782844666c6230812bb471e812
Ravi <kkiran.ror@gmail.com>
2020-03-09 03:38:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function destructivelyPrependKitten(name){
    kittens.unshift(name)
    return kittens
 }
+
+
+function destructivelyRemoveLastKitten(name){
+   kittens.unshift(name)
+   return kittens
+}
+function appendKitten(name){
+   kittens.unshift(name)
+   return kittens
+}
