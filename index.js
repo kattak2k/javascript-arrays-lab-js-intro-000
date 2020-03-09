cc5f71ee6ca61bba1f0380c7973c4abe65dcc2e2
Ravi <kkiran.ror@gmail.com>
2020-03-09 03:45:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ function destructivelyPrependKitten(name){
    return kittens
 }
 
-
 function destructivelyRemoveLastKitten(name){
    kittens.pop(name)
    return kittens
 }
+
+function destructivelyRemoveFirstKitten(name){
+   kittens.pop(name)
+   return kittens
+}
+
 function appendKitten(name){
    kittens.unshift(name)
    return kittens
