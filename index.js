6a0f4036b14f750bb0c5401363c1c89c666cde14
Ravi <kkiran.ror@gmail.com>
2020-03-09 03:49:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ function appendKitten(name){
 function prependKitten(name){
 return [ name , ...kittens]
 }
+
 function removeLastKitten(){
-   kittens.unshift(name)
-   return kittens
+
+   return kittens.slice(0, kittens.length - 1)
 }
 function removeFirstKitten(){
    kittens.unshift(name)
